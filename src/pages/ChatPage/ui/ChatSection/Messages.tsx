26e0b6d268f73019815d13d153019bbdf445f7b8
Кайдаров Павел <pavel.kaydarov@fabit.ru>
2024-05-25 12:08:48 +0300

    add avatar

--- src/pages/ChatPage/ui/ChatSection/Messages.tsx
+++ src/pages/ChatPage/ui/ChatSection/Messages.tsx
@@ -20,44 +20,60 @@ export const Messages = (props: IMessagesProps) => {
         <div>
             <div className="messages-list">
                 {chatInfo.step >= 0 && (
-                    <p className="from-them">
-                        {chat.messages[0].text()}
-                    </p>
+                    <div className="messages-list__step">
+                        <img src={chat.avatar} alt="avatar" />
+                        <p className="from-them">
+                            {chat.messages[0].text()}
+                        </p>
+                    </div>
+
                 )}
                 {chatInfo.step >= 1 && (
                     <>
                         <p className="from-me">{chatInfo.messages?.[0]}</p>
-                        <p className="from-them">{chat.messages[1].text(chatInfo.name)}</p>
+                        <div className="messages-list__step">
+                            <img src={chat.avatar} alt="avatar" />
+                            <p className="from-them">{chat.messages[1].text(chatInfo.name)}</p>
+                        </div>
                     </>
                 )}
                 {chatInfo.step >= 2 && (
                     <>
                         <p className="from-me">{chatInfo.messages?.[1]}</p>
-                        <p className="from-them">
-                            {
-                                chat.messages[2].text(chatInfo.name, chatInfo.selectedOption)
-                            }
-                        </p>
+                        <div className="messages-list__step">
+                            <img src={chat.avatar} alt="avatar" />
+                            <p className="from-them">
+                                {
+                                    chat.messages[2].text(chatInfo.name, chatInfo.selectedOption)
+                                }
+                            </p>
+                        </div>
                     </>
                 )}
                 {chatInfo.step >= 3 && (
                     <>
                         <p className="from-me">{chatInfo.messages?.[2]}</p>
-                        <p className="from-them">
-                            {
-                                chat.messages[3].text(chatInfo.name, chatInfo.selectedOption)
-                            }
-                        </p>
+                        <div className="messages-list__step">
+                            <img src={chat.avatar} alt="avatar" />
+                            <p className="from-them">
+                                {
+                                    chat.messages[3].text(chatInfo.name, chatInfo.selectedOption)
+                                }
+                            </p>
+                        </div>
                     </>
                 )}
                 {chatInfo.step >= 4 && (
                     <>
                         <p className="from-me">{chatInfo.messages?.[3]}</p>
-                        <p className="from-them">
-                            {
-                                chat.messages[4].text(chatInfo.name, chatInfo.selectedOption)
-                            }
-                        </p>
+                        <div className="messages-list__step">
+                            <img src={chat.avatar} alt="avatar" />
+                            <p className="from-them">
+                                {
+                                    chat.messages[4].text(chatInfo.name, chatInfo.selectedOption)
+                                }
+                            </p>
+                        </div>
                     </>
                 )}
             </div>
